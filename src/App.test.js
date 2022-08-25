bbe1a6a86d6e83282414dce643d6c86fe1318305
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-25 11:27:07 +0530

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Where to write test specs? here in this file
+// How to Write?
+//  by following Testing Pattern: AAA (Arrange, Act, Assert)
 
+// Arrange 
+import { render, screen } from '@testing-library/react';
+import App from './App'; // taking up the comp for testing 
+
+// Test Spec / Test case 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
